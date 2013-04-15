a0e6f70b79166fab370b768fcfeb8615abc966a6
maojj <mao-jiajun@163.com>
2013-04-16 00:27:52 +0800

    Initial Commit

---
+++ MJJCore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MJJCore
+//
+//  Created by maojj on 13-4-16.
+//  Copyright (c) 2013年 maojj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MJJCore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MJJCore
+//
+//  Created by maojj on 13-4-16.
+//  Copyright (c) 2013年 maojj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
